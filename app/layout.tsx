df0ff4954a169ca5d95cb4851ba22fd45c48acbf
marcusgeorgievski <marcusgeorgievski@outlook.com>
2025-07-23 12:40:53 -0400

    temp redirect away

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,12 +2,15 @@ import "./global.css";
 import { RootProvider } from "fumadocs-ui/provider";
 import { Inter } from "next/font/google";
 import type { ReactNode } from "react";
+import { redirect } from "next/navigation";
 
 const inter = Inter({
   subsets: ["latin"],
 });
 
 export default function Layout({ children }: { children: ReactNode }) {
+  redirect("https://marcusgeorgievski.com"); // temporary redirect, no access to the app yet
+
   return (
     <html lang="en" className={inter.className} suppressHydrationWarning>
       <body className="flex flex-col min-h-screen">
